3d2d9ea2900ee861644a6fb2d227825d0f000285
Patrick Murray-John <patrickmjchnm@gmail.com>
2012-03-01 17:35:26 -0500

    Moves the div wrapper into the BlocksManager to encourage consistency.
    adds a class on the wrapper based on the name of the block, using
    Inflector::underscore.
    
    For CSS usage, I wonder if it might be good to add an Inflector::dashes
    method that just does dashes instead of underscores?

--- libraries/Blocks/BlocksManager.php
+++ libraries/Blocks/BlocksManager.php
@@ -12,12 +12,12 @@ class BlocksManager
     public $request;
     public $blocks = array();
     public $html= '';
-    
+
     public function setRequest($request)
     {
         $this->request = $request;
     }
-    
+
     public function addBlocks()
     {
         $requestInfo = $this->request->getParams();
@@ -31,24 +31,27 @@ class BlocksManager
             $this->blocks[] = new $class($this->request, $blockConfig);
         }
     }
-    
+
     public function renderBlocks()
     {
         foreach($this->blocks as $block) {
             try {
                 $html = $block->render();
                 if($html) {
+                    $blockClass = 'block_' . Inflector::underscore(get_class($block));
+                    $this->html .= "<div class='block $blockClass'>";
                     $this->html .= $html;
+                    $this->html .= "</div>";
                 }
             } catch (Exception $e) {
-                _log($e->getMessage());
+                _log($e);
             }
         }
     }
-        
+
     public function getHtml()
     {
         return $this->html;
     }
-    
+
 }
